d52d96083925cfed86bcb3a341dd1fd27ea5f855
Maxence Bourgeaux <maxence.bourgeaux@etu.univ-lehavre.fr>
2021-01-29 10:23:11 +0100

    "Classe Circle vu en cours"

--- src/circle.js
+++ src/circle.js
@@ -1,10 +1,75 @@
-// TODO
 // A `Circle` class/function that can be invoked as :
 // const c = new Circle();
 // It should contain at least 3 hidden fields `x`, `y`, and `radius`.
-export function Circle() {
-  // TODO
+export class Circle {
+  #x;
+  #y;
+  #radius;
+
+  constructor(x = 0, y = 0, radius = 0) {
+    this.#x = x;
+    this.#y = y;
+    this.#radius = radius;
+    Object.freeze(this);
+  }
+
+  get coords() {
+    return [this.#x, this.#y];
+  }
+
+  moveTo(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {throw  new Error('x and y should be numbers');}
+    this.#x = x;
+    this.#y = y;
+  }
+
+  get radius() {
+    return this.#radius;
+  }
+
+  set radius(radius) {
+    if (typeof radius !== 'number') {throw  new Error('radius should be a numbers');}
+    if (radius < 0) {throw  new Error('radius should be a positive numbers');}
+    this.#radius = radius;
+  }
+
+  get area() {
+    return this.#radius * this.#radius * Math.PI;
+  }
+
+  set area(area) {
+    if (typeof area !== 'number') {throw  new Error('area should be a numbers');}
+    if (area < 0) {throw  new Error('area should be a positive numbers');}
+    this.radius = Math.sqrt(area / Math.PI);
+  }
+
+  static distance(circle1, circle2) {
+    const [x1, y1] = circle1.coords;
+    const [x2, y2] = circle2.coords;
+    const dx = x1- x2;
+    const dy = y1- y2;
+
+    return Math.sqrt(dx**2  +  dy**2);
+    // sqrt ( ( x2 - x1) ^2   + (y2 - y1) ^2 )
+  }
+
+  static doIntersect(circle1, circle2) {
+    return (circle1.radius + circle2.radius) > Circle.distance(circle1, circle2);
+  }
 }
 
+/*
+export class Circle2 extends Circle {
+  #toto
 
+  constructor(x, y, radius, toto) {
+    super(x, y, radius);
+    this.#toto = toto;
+    Object.freeze(this);
+  }
 
+  get toto() {
+    return this.#toto;
+  }
+}
+ */
--- src/circle.test.js
+++ src/circle.test.js
@@ -10,12 +10,13 @@ describe('Circle', () => {
 
   test('Not enumerable', () => {
     let c1 = new Circle();
+    // eslint-disable-next-line no-unused-vars
     for (const prop in c1) {
       fail();
     }
   });
 
-  test(' 3 parameters, but 2 attributs', () => {
+  test(' 3 parameters, but 2 attributes', () => {
     const x = 1;
     const y = 2;
     const radius = 5;
@@ -34,8 +35,8 @@ describe('Circle', () => {
 
   test('coords is readonly', () => {
     let c = new Circle(1, 1);
-    c.coodrs = [0, 0];
-    expect(c.coords).toStrictEqual([1, 1]);
+    expect(() => (c.toto = 42)).toThrow();
+    expect(() => (c.coords = [0, 0])).toThrow();
   });
 
   test('only moveTo(x,y) can change coordinates', () => {
@@ -44,12 +45,20 @@ describe('Circle', () => {
     c.coords.map((z) => expect(z).toBe(0));
   });
 
+  test('moveTo(x,y) only numbers', () => {
+    let c = new Circle(1, 1);
+    c.moveTo(0, 0);
+    expect(() => (c.moveTo("a", 5))).toThrow();
+  });
+
   test('radius can be changed to positive values only', () => {
     let c = new Circle(0, 0, 0);
     c.radius = 12;
     expect(c.radius).toBe(12);
 
     expect(() => (c.radius = -12)).toThrow();
+
+    expect(() => (c.radius = "test")).toThrow();
   });
 
   test('area is read/write and modifies the radius. Positive values only', () => {
@@ -62,12 +71,13 @@ describe('Circle', () => {
     expect(c.radius).toBe(10);
 
     expect(() => (c.area = -12)).toThrow();
+
+    expect(() => (c.area = "toto")).toThrow();
   });
 
   test('distance', () => {
     const c1 = new Circle(0, 0, 1);
     const c2 = new Circle(10, 10, 1);
-    // SQRT(2) * 10 =
     const expectedDistance = 10 * Math.sqrt(2);
     expect(Circle.distance(c1, c2)).toBe(expectedDistance);
   });
